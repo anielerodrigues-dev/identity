caa0b08c97a2c1bab65db0cd857e44b26611f7be
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-06-05 18:35:59 -0300

    Add files via upload

---
+++ identidade.java
@@ -0,0 +1,18 @@
+public class identidade {
+    public static void main(String[] args) {
+    
+    String name = "Aniele Rodrigues";
+    String state = "F";
+    char gender = 'F';
+    int age = 26;
+    boolean organDonor = true;
+
+        System.out.println(name);
+        System.out.println(state);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(organDonor);
+
+
+    }
+}
